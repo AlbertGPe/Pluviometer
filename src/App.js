876ed75e41d557650f5f787381bfa9e6e883d1e1
AlbertGPe <albertgarciapedrosa@gmail.com>
2024-06-11 20:12:03 +0200

    DateSelector

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import CitySelector from "./components/CitySelector";
+import DateSelector from "./components/DateSelector";
 import Pluviometer from "./components/Pluviometer";
 import cities from './data/cities.json'
 
 function App() {
   return (
     <div className="App">
-      <CitySelector cities={cities} selectedCity={'Madrid'}/>
+      <DateSelector selectedDate={'Today'}/>
     </div>
   );
 }
